c4c723b6c28165b066765350c5007d13eac5e467
Fahed <36611526+fahed670@users.noreply.github.com>
2026-08-13 13:54:14 +0400

    Add Space ID location model

---
+++ mobile/lib/core/models/device_location.dart
@@ -0,0 +1,11 @@
+class DeviceLocation {
+  const DeviceLocation({
+    required this.latitude,
+    required this.longitude,
+    this.accuracyMeters,
+  });
+
+  final double latitude;
+  final double longitude;
+  final double? accuracyMeters;
+}
